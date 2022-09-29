b49e6b0d73f5bb0f3444b56c76c70943a5f0d5dc
Elam <dexiruslander@gmail.com>
2022-09-29 09:34:14 +0200

    remplacement des class par id

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let zonePourLaVoiture = document.getElementById('zonePourLaVoiture');
+let corpsDeLaVoiture = document.getElementById('corpsDeLaVoiture');
+let fenetreGauche = document.getElementById('fenetreGauche');
+let fenetreDroite = document.getElementById('fenetreDroite');
+let roueGauche = document.getElementById('roueGauche');
+let exterieureRoue = document.getElementById('exterieureRoue');
+let interieureRoue = document.getElementById('interieureRoue');
+let etoile = document.getElementById('etoile');
+let roueDroite = document.getElementById('roueDroite');
+let exterieureRoue = document.getElementById('exterieureRoue');
+let interieureRoue = document.getElementById('interieureRoue');
+let etoile = document.getElementById('etoile');
+
+
