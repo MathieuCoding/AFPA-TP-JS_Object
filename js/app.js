178c94b5b0f2c90f37c8781395bb5c1d8192112e
Mattpllt <103026412+Mattpllt@users.noreply.github.com>
2022-09-29 13:53:05 +0200

    couleurs de voiture de base

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*_________________*/
 
 ////// Customisation possibles
-/* LES NOMS DES ÉLÉMENT DE LA VOITURE À MURPHY
+/* LES NOMS DES ï¿½Lï¿½MENT DE LA VOITURE ï¿½ MURPHY
  * - carosserie
  * - fenetres
  * - roues
@@ -20,49 +20,53 @@
 //voiture.roues= "a ";
 //voiture.jantes = " a";
 
-let voiture = {
-    carosserie: "pink",
-    fenetres: "blue",
-    roues: "red",
-    jantes: "purple"
-};
+// let voiture = {
+//     carosserie: "pink",
+//     fenetres: "blue",
+//     roues: "red",
+//     jantes: "purple"
+// };
 
-delete voiture.fenetres;
-delete voiture.roues;
+// delete voiture.fenetres;
+// delete voiture.roues;
 
-voiture.fenetres = {
-    fGauche : "blue",
-    fDroite : "pink"
-}
+// voiture.fenetres = {
+//     fGauche : "blue",
+//     fDroite : "pink"
+// }
 
-voiture.roues = {
-    rGauche: "blue",
-    rDroite : "pink"
-}
+// voiture.roues = {
+//     rGauche: "blue",
+//     rDroite : "pink"
+// }
 
 
-let tab = Object.keys(voiture);
+// let tab = Object.keys(voiture);
 
-for (i = 0; i < tab.length; i++) {
-    switch (tab[i]) {
-        case "fenetres":
-            let subTab = Object.keys(voiture[tab[i]]);
-            for (var j = 0; j < subTab.length; j++) {
-                console.log(subTab[j], document.getElementsByClassName(subTab[j])[0].classList[1])
-                document.getElementsByClassName(subTab[j])[0].classList[1].style["background-color"] = subTab[j];
-            }
-            break;
-        case "roues":
-            for (var j = 0; j < voiture[tab[i]].length; j++) {
-                document.getElementsByClassName(tab[i][j])[0].style["background-color"] = voiture[tab[i]];
-            }
-            document.getElementsByClassName(tab[i])[0].style["background-color"] = voiture[tab[i]];
-            break;
-        default:
-            console.log(document.getElementsByClassName(tab[i])[0])
-            document.getElementsByClassName(tab[i])[0].style["background-color"] = voiture[tab[i]];
-            break;
-    }
+// for (i = 0; i < tab.length; i++) {
+//     for (j = 0; j < tab[i].length; i++) {
+//         document.getElementsByClassName(tab[j])[0].style["background-color"] = voiture[tab[j]];
+//     }
+    
+    // switch (tab[i]) {
+    //     case "fenetres":
+    //         let subTab = Object.keys(voiture[tab[i]]);
+    //         for (var j = 0; j < subTab.length; j++) {
+    //             console.log(subTab[j], document.getElementsByClassName(subTab[j])[0].classList[1])
+    //             document.getElementsByClassName(subTab[j])[0].classList[1].style["background-color"] = subTab[j];
+    //         }
+    //         break;
+    //     case "roues":
+    //         for (var j = 0; j < voiture[tab[i]].length; j++) {
+    //             document.getElementsByClassName(tab[i][j])[0].style["background-color"] = voiture[tab[i]];
+    //         }
+    //         document.getElementsByClassName(tab[i])[0].style["background-color"] = voiture[tab[i]];
+    //         break;
+    //     default:
+    //         console.log(document.getElementsByClassName(tab[i])[0])
+    //         document.getElementsByClassName(tab[i])[0].style["background-color"] = voiture[tab[i]];
+    //         break;
+    // }
 }
 
 
